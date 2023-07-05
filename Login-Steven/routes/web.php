f72b5039b0e6c7ac2e3808f4fae0eb570c7e0996
(StevenYoe) <(Stevenyoe80@gmail.com)>
2023-07-05 12:07:25 +0700

    set route

--- Login-Steven/routes/web.php
+++ Login-Steven/routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/awal', function () {
+    return view('index');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/akhir', [App\Http\Controllers\HomeController::class, 'test'])->name('akhir');
